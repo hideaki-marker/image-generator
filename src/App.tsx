b78c06bba6bda3311bc074d3e27eb19c32452f09
hideaki-marker-admin <sunday.slowlife@gmail.com>
2024-12-04 23:43:23 +0900

    画像の表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [generatedImage, setGeneratedImage] = useState<string | null>(null);
+  const [prompt, setPrompt] = useState("");
 
+  const apiHost = "https://api.stability.ai";
+  const engineId = "stable-diffusion-v1-6";
+  const apiKey= import.meta.env.VITE_STABLITY_API_KEY;
+  const handleGenerateImage = async () => {
+    const response = await fetch(
+      `${apiHost}/v1/generation/${engineId}/text-to-image`,
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      text_prompts: [
+        {
+          text: prompt,
+        },
+      ],
+      cfg_scale: 7,
+      height: 1024,
+      width: 1024,
+      steps: 30,
+      samples: 1,
+    }),
+  }
+    );
+    if(!response.ok){
+      throw Error('Failed to generate image');
+    }
+    const responseJson = await response.json();
+    const base64Image = responseJson.artifacts[0].base64;
+    setGeneratedImage(`data:image/png;base64,${base64Image}`);
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div>
+          <input type="text"  className="border" value={prompt} onChange={(e) => setPrompt(e.target.value)}/>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={handleGenerateImage}>
+            生成
+          </button>
+          {generatedImage &&(
+            <img src={generatedImage} alt="生成された画像" />
+          )}
+          
+        </div>
     </>
   )
 }
